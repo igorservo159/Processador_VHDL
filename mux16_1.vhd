f5829a5aa34a27766cbd22be57a71d6a8d9019bc
jpxx-arts <jpramalho123@hotmail.com>
2024-08-12 22:33:46 -0300

    fixing issues

--- mux16_1.vhd
+++ mux16_1.vhd
@@ -6,6 +6,9 @@ package bus_array_pkg is
         type bus_array is array(15 downto 0) of std_logic_vector(15 downto 0);
 end package;
 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use work.bus_array_pkg.all;
 
 entity mux16_1 is
